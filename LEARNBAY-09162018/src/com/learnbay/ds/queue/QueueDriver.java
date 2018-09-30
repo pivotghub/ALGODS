c471517992c71bf24b702bdae3074c730e2f6fa0
pivotghub <zerotodolt@gmail.com>
2018-09-30 16:47:56 +0530

    Test cases for circular queue

--- LEARNBAY-09162018/src/com/learnbay/ds/queue/QueueDriver.java
+++ LEARNBAY-09162018/src/com/learnbay/ds/queue/QueueDriver.java
@@ -5,7 +5,7 @@ public class QueueDriver {
 	
 	public static void main(String[] args){
 		
-		Queue queue = new Queue(5);
+		/*Queue queue = new Queue(5);
 		queue.enQueue(1);
 		queue.enQueue(1);
 		queue.enQueue(1);
@@ -17,6 +17,36 @@ public class QueueDriver {
 		queue.deQueue();
 		queue.deQueue();
 		
-		queue.print();
+		queue.print();*/
+		
+		
+		
+		//CircularQueue Driver not a generic one.
+		CircularQueue circularQueue = new CircularQueue(8);
+
+		circularQueue.enQueue(15);
+		circularQueue.enQueue(16);
+		circularQueue.enQueue(17);
+		circularQueue.enQueue(18);
+		circularQueue.enQueue(19);
+		circularQueue.enQueue(20);
+		circularQueue.enQueue(21);
+		circularQueue.enQueue(22);
+
+		System.out.println("Full Circular Queue");
+		circularQueue.print();
+
+		System.out.print("Dequeued following element from circular Queue ");
+		circularQueue.deQueue();
+		
+		circularQueue.enQueue(23);
+		System.out.println("After enqueueing circular queue with element having value 23");
+		circularQueue.print();
+		circularQueue.deQueue();
+		circularQueue.enQueue(233);
+		circularQueue.print();
+		circularQueue.enQueue(12);
+		circularQueue.print();
+		
 	}
 }
